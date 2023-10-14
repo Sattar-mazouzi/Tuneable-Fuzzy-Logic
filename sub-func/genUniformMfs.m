19a6758ba1224c1d6fd19f0f6df95611548a5b0b
Abdesattar <mazouziabdou6@gmail.com>
2023-10-14 19:54:40 +0100

    function to build a FIS with uniform trapmf and trimf

--- sub-func/genUniformMfs.m
+++ sub-func/genUniformMfs.m
@@ -1,13 +1,14 @@
 function [out1, out2 ]= genUniformMfs(fuzzyVarParams, mf_types)
-mf_parameters = []; 
+mfs = {[],[],[]}; 
 var1  = fuzzyVarParams(1,:); 
 var2 = fuzzyVarParams(2,:);
 var3 = fuzzyVarParams(3,:);
 mf_types_cell = {[mf_types(1:var1(2))],[mf_types(var1(2)+1:var1(2)+var2(2))], ...
     [mf_types(var1(2)+var2(2)+1:var1(2)+var2(2)+var3(2))]} ;
 varit = size(fuzzyVarParams);               % get the number of fuzzy variable 
-idx = 1;
+
 for i = 1:varit(1)                          % iterate through the number of Variables
+    idx = 1;
     fuzzyVar = fuzzyVarParams(i,:); % get variable parameter
     step = fuzzyVar(1)/(fuzzyVar(2)-1);
     mf_type = mf_types_cell{i}; 
@@ -15,17 +16,17 @@ for i = 1:varit(1)                          % iterate through the number of Vari
         indic = step*j; 
         if mf_type(j) ==0
             mfparams = [indic-2*step, indic-step, indic];
-            mf_parameters(idx:idx+2) = mfparams ; 
+            mfs{i}(idx:idx+2) = mfparams ; 
             idx = idx +3; 
         elseif mf_type(j) == 1 
             mfparams = [indic-2*step, indic-1.4*step,indic-0.6*step, indic];
-            mf_parameters(idx:idx+3) = mfparams ; 
+            mfs{i}(idx:idx+3) = mfparams ; 
             idx = idx +4;
         end
 
     end
 
 end
-out1= mf_parameters; 
+out1 = mfs ;
 out2 = mf_types_cell; 
 end 
--- tunebale_flc.m
+++ tunebale_flc.m
@@ -1,4 +1,4 @@
-function out = tunebale_flc(input1,input2,output ) 
+function out = tunebale_flc(input1,input2,output,mf_type ) 
 
 %% extract fuzzy variable information 
 
@@ -14,18 +14,41 @@ out_name = output.name ;         % output name, fuzzy range and  the number of
 out_range = output.range;        % membership functions 
 out_mfNumber = output.MfNumber; 
 
-fuzzyVarParams = [in1_range(1), in1_mfNumber;   % Fuzzy variable paramters 
-                  in2_range(1), in2_mfNumber;
-                  out_range(1), out_mfNumber;] ; 
+fuzzyVarParams = [in1_range(2), in1_mfNumber;   % Fuzzy variable paramters 
+                  in2_range(2), in2_mfNumber;
+                  out_range(2), out_mfNumber;] ; 
 fuzzyVarNames = [in1_name, in2_name, out_name];  % FIS variable names  
+mf_types = mf_type;
+%% generate a uniformly distributed memebership function
+% generate MFs parameters and MF types
+[mf_parameters, mf_types_cell] = genUniformMfs(fuzzyVarParams,mf_types); 
 
-%% generate a uniformly distributed memebership function 
- 
-mf_parameters, mf_types_cell = genUniformMfs(mf_types, fuzzyVarParams); 
+% generate MFs names 
+mf_names  = genMfNames(mf_types_cell); 
+
+%% Create Fis 
+fis = mamfis('Name','fis');
+fis = addInput(fis,  in1_range, 'Name', in1_name);
+fis = addInput(fis, in2_range, "Name",in2_name); 
+fis = addOutput(fis, out_range,'Name', out_name); 
 
 
-
-
-
-
+for i = 1:length(fuzzyVarNames)         % iterate through the number of Variables
+    idx = 1;
+    fuzzyVar = fuzzyVarParams(i,:); % get variable parameter
+    mf_type = mf_types_cell{i};
+    sel_var = mf_parameters{i};
+    for j = 1:fuzzyVar(2)                    % iterate through the number of MFs 
+        if mf_type(j) ==0
+            mfparams = sel_var(idx:idx+2);  
+            fis = addMF(fis,fuzzyVarNames(i), 'trimf', mfparams, "Name",mf_names{i}(j));
+            idx = idx +3; 
+        elseif mf_type(j) == 1 
+            mfparams = sel_var(idx:idx+3); 
+            fis = addMF(fis,fuzzyVarNames(i), 'trapmf', mfparams, "Name",mf_names{i}(j));
+            idx = idx +4;
+        end
+    end
+end
+out = fis; 
 end
